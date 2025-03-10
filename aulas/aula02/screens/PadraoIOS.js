1739331c7bd53c750eee64f30f2d383211389a45
CaioAleixo <caio.aleixo@iesb.edu.br>
2025-03-10 19:43:19 -0300

    aula02

--- aulas/aula02/screens/PadraoIOS.js
+++ aulas/aula02/screens/PadraoIOS.js
@@ -1,12 +1,14 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 function PadraoIOS () {
     return (
-        <View style={{ flex: 1,
-         justifyContent: "space-between"
+        <View style={{ 
+            flex: 1,
+            justifyContent: "space-between"
          }}>
-
-            <View style={{ height: 64, backgroundColor: "gray" }}></View>
+            <View style={{ height: 100, backgroundColor: "gray", justifyContent: "center", alignItems: "center" }}>
+                <Text>Meu App</Text>
+            </View>
             <View style={{ height: 64, backgroundColor: "blue"}}></View>
 
         </View>
